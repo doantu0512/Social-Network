a6faecaf1088a2770b9f07dd759f5491080091fb
phong2002 <phong.luminh02@gmail.com>
2023-02-21 14:29:22 +0700

    update : create Storage

--- SocialNetwork/Server/src/main/java/kl/socialnetwork/validations/serviceValidation/servicesImpl/StorageServiceImpl.java
+++ SocialNetwork/Server/src/main/java/kl/socialnetwork/validations/serviceValidation/servicesImpl/StorageServiceImpl.java
@@ -3,11 +3,15 @@ package kl.socialnetwork.validations.serviceValidation.servicesImpl;
 import kl.socialnetwork.utils.responseHandler.exceptions.StorageException;
 import kl.socialnetwork.validations.serviceValidation.services.StorageService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
@@ -48,12 +52,24 @@ public class StorageServiceImpl implements StorageService {
             }
         }
 
-
-
-
-
-
         return paths;
     }
+
+    @Override
+    public Resource loadFile(String filename) {
+        try {
+            Path file = Paths.get(folder).resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
 }
 
